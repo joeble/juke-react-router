8c7db50fbcb388c833de57b5bceabe6591de12ba
Erik Shuttlesworth <erik.shuttlesworth@gmail.com>
2017-08-23 10:30:44 -0500

    finished section 9

--- browser/react/components/SingleArtist.js
+++ browser/react/components/SingleArtist.js
@@ -2,7 +2,7 @@ import React from 'react';
 import axios from 'axios';
 import Songs from './Songs';
 import AllAlbums from './AllAlbums';
-import {Link} from 'react-router-dom';
+import {HashRouter as Router, Route, Link} from 'react-router-dom';
 
 export default class SingleAlbum extends React.Component {
 
@@ -39,24 +39,29 @@ export default class SingleAlbum extends React.Component {
     })
       .then(() => console.log(this.state))
       .catch(console.error.bind(console));
-
   }
 
   render () {
+    const artist = this.state.artist; // or however you've named it
+    const albums = this.state.artistAlbums;
+    const songs = this.state.artistSongs;
 
-      const artist = this.state.artist; // or however you've named it
+    return (
+      <div>
+        <h3>{ artist.name }</h3>
+        <ul className="nav nav-tabs">
+          <li><Link to={`/artists/${artist.id}/albums`}>ALBUMS</Link></li>
+          <li><Link to={`/artists/${artist.id}/songs`}>SONGS</Link></li>
+        </ul>
 
-      return (
-        <div>
-          <h3>{ artist.name }</h3>
-          <ul className="nav nav-tabs">
-            <li><Link to="TODO">ALBUMS</Link></li>
-            <li><Link to="TODO">SONGS</Link></li>
-          </ul>
-
-          {/* Routes will go here! */}
-        </div>
-      );
-    }
-
+        {/* Routes will go here! */}
+        <Router>
+          <div>
+            <Route path="/artists/:artistId/albums" render={() => <AllAlbums albums={albums} />} />
+            <Route path="/artists/:artistId/songs" render={() => <Songs songs={songs} />} />
+          </div>
+        </Router>
+      </div>
+    );
   }
+}
