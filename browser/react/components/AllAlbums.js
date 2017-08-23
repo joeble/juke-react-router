cfa6b75deaac138bae7f5d4fa04a2089c65bfc45
Joseph Knoebel <knoebelja@gmail.com>
2017-08-23 11:13:12 -0400

    Starting 9. Writing Sub-Routes

--- browser/react/components/AllAlbums.js
+++ browser/react/components/AllAlbums.js
@@ -1,48 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
-import axios from 'axios';
 
-export default class AllAlbums extends Component {
+export default function AllAlbums(props) {
 
-  constructor() {
-    super();
-    this.state = {
-      albums: []
-    };
-  }
+  const albums = props.albums;
 
-  componentDidMount () {
-    axios.get('/api/albums/')
-      .then(res => res.data)
-      .then(albums => {
-        this.setState({ albums })
-      });
-  }
-
-  render () {
-    const albums = this.state.albums;
-
-    return (
-      <div>
-        <h3>Albums</h3>
-        <div className="row">
-        {
-          albums.map(album => (
-            <div className="col-xs-4" key={ album.id }>
-              <Link className="thumbnail" to={`/albums/${album.id}`} >
-                <img src={ album.imageUrl } />
-                <div className="caption">
-                  <h5>
-                    <span>{ album.name }</span>
-                  </h5>
-                  <small>{ album.songs.length } songs</small>
-                </div>
-              </Link>
-            </div>
-          ))
-        }
-        </div>
+  return (
+    <div>
+      <h3>Albums</h3>
+      <div className="row">
+      {
+        albums.map(album => (
+          <div className="col-xs-4" key={ album.id }>
+            <Link className="thumbnail" to={`/albums/${album.id}`} >
+              <img src={ album.imageUrl } />
+              <div className="caption">
+                <h5>
+                  <span>{ album.name }</span>
+                </h5>
+                <small>{ album.songs.length } songs</small>
+              </div>
+            </Link>
+          </div>
+        ))
+      }
       </div>
-    );
-  }
+    </div>
+  );
+
 }
--- browser/react/components/SingleArtist.js
+++ browser/react/components/SingleArtist.js
@@ -1,37 +1,62 @@
 import React from 'react';
 import axios from 'axios';
+import Songs from './Songs';
+import AllAlbums from './AllAlbums';
+import {Link} from 'react-router-dom';
 
 export default class SingleAlbum extends React.Component {
 
   constructor(){
     super();
     this.state = {
-      selectedArtist: {}
+      artist: {},
+      artistAlbums: [],
+      artistSongs: []
     }
   }
 
   componentDidMount () {
 
-  const artistId = this.props.match.params.artistId;
+    const artistId = this.props.match.params.artistId;
 
-  axios.get(`/api/artists/${artistId}`)
-    .then(res => res.data)
-    .then(artist => this.setState({
-      selectedArtist: artist
-    }));
+    const getArtist = axios.get(`/api/artists/${artistId}`),
+          getAlbums = axios.get(`/api/artists/${artistId}/albums`),
+          getSongs = axios.get(`/api/artists/${artistId}/songs`);
+
+    Promise.all([getArtist, getAlbums, getSongs])
+    .then(([artist, artistAlbums, artistSongs]) => {
+
+      // filters out albums with 0 songs
+      let correctAlbums = artistAlbums.data.filter(function(album) {
+        return album.songs.length > 0;
+      });
+
+      this.setState({
+        artist: artist.data,
+        artistAlbums: correctAlbums,
+        artistSongs: artistSongs.data
+      })
+    })
+      .then(() => console.log(this.state))
+      .catch(console.error.bind(console));
+
   }
 
-  render(){
+  render () {
 
-    const artist = this.state.selectedArtist;
+      const artist = this.state.artist; // or however you've named it
 
-    return (
-      <div>
-        <h3>ARTIST NAME</h3>
-        <h4>ALBUMS</h4>
-        <h4>SONGS</h4>
-      </div>
-    );
+      return (
+        <div>
+          <h3>{ artist.name }</h3>
+          <ul className="nav nav-tabs">
+            <li><Link to="TODO">ALBUMS</Link></li>
+            <li><Link to="TODO">SONGS</Link></li>
+          </ul>
+
+          {/* Routes will go here! */}
+        </div>
+      );
+    }
+
   }
-
-}
